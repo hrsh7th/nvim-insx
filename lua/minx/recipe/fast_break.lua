b74cf6480cb943838bf17b6a3bbe1a75ef84e24d
hrsh7th <629908+hrsh7th@users.noreply.github.com>
2023-01-14 00:34:43 +0900

    Improve fast_break undo behavior

--- lua/minx/recipe/fast_break.lua
+++ lua/minx/recipe/fast_break.lua
@@ -10,24 +10,22 @@ local function fast_break(option)
   return {
     ---@param ctx minx.ActionContext
     action = function(ctx)
+      local open_indent = helper.indent.get_current_indent()
+      ctx.send(ctx.char)
+      ctx.send(helper.indent.ajudst_keys(helper.indent.get_current_indent(), open_indent .. helper.indent.get_one_indent()))
+      local row, col = ctx.row(), ctx.col()
       local close_pos = helper.search.get_pair_close(option.open_pat, option.close_pat)
-      if close_pos and ctx.row() == close_pos[1] then
-        local open_indent = helper.indent.get_current_indent()
-        local row, col = ctx.row(), ctx.col()
-        ctx.move(close_pos[1], close_pos[2])
-        ctx.send(ctx.char)
-        ctx.send(helper.indent.ajudst_keys(helper.indent.get_current_indent(), open_indent))
-        ctx.move(row, col)
-        ctx.send(ctx.char)
-        ctx.send(helper.indent.ajudst_keys(helper.indent.get_current_indent(), open_indent .. helper.indent.get_one_indent()))
-      else
-        ctx.send(ctx.char)
-      end
+      assert(close_pos)
+      ctx.move(close_pos[1], close_pos[2])
+      ctx.send(ctx.char)
+      ctx.send(helper.indent.ajudst_keys(helper.indent.get_current_indent(), open_indent))
+      ctx.move(row, col)
     end,
     ---@param ctx minx.Context
     enabled = function(ctx)
       local before = helper.regex.match(ctx.before(), option.open_pat .. [[\s*$]])
-      return not helper.syntax.in_string_or_comment() and before
+      local close_pos = helper.search.get_pair_close(option.open_pat, option.close_pat)
+      return not helper.syntax.in_string_or_comment() and before and close_pos and close_pos[1] == ctx.row()
     end,
   }
 end
