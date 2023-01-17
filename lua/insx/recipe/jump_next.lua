10e92c54d764cd80732f343ea20c5f31323cf2fd
hrsh7th <629908+hrsh7th@users.noreply.github.com>
2023-01-17 22:03:38 +0900

    Use helper.position.gt

--- lua/insx/recipe/jump_next.lua
+++ lua/insx/recipe/jump_next.lua
@@ -19,7 +19,7 @@ local function jump_next(option)
       for _, pat in ipairs(jump_pat) do
         local pos = helper.search.get_next(pat)
         if pos then
-          if pos[1] < curr_pos[1] or (pos[1] == curr_pos[1] and pos[2] < curr_pos[2]) then
+          if helper.position.gt(pos, curr_pos) then
             curr_pos = pos
           end
         end
