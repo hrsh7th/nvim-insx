18ca567dc688e710161f375606516f67def0db49
hrsh7th <629908+hrsh7th@users.noreply.github.com>
2023-01-19 01:26:27 +0900

    Add preset tests

--- lua/insx/preset/standard.lua
+++ lua/insx/preset/standard.lua
@@ -5,21 +5,30 @@ local standard = {}
 
 function standard.setup()
   -- quotes
-  for _, quote in ipairs({ '"', "'", "`" }) do
-    insx.add(quote, require('insx.recipe.jump_next')({
-      jump_pat = {
-        [[\\\@<!\%#]] .. esc(quote) .. [[\zs]]
-      },
-    }))
-    insx.add(quote, require('insx.recipe.auto_pair')({
-      open = quote,
-      close = quote,
-      ignore_pat = [[\\\%#]],
-    }))
-    insx.add('<BS>', require('insx.recipe.delete_pair')({
-      open_pat = esc(quote),
-      close_pat = esc(quote),
-    }))
+  for _, quote in ipairs({ '"', "'", '`' }) do
+    insx.add(
+      quote,
+      require('insx.recipe.jump_next')({
+        jump_pat = {
+          [[\\\@<!\%#]] .. esc(quote) .. [[\zs]],
+        },
+      })
+    )
+    insx.add(
+      quote,
+      require('insx.recipe.auto_pair')({
+        open = quote,
+        close = quote,
+        ignore_pat = [[\\\%#]],
+      })
+    )
+    insx.add(
+      '<BS>',
+      require('insx.recipe.delete_pair')({
+        open_pat = esc(quote),
+        close_pat = esc(quote),
+      })
+    )
   end
 
   -- pairs
@@ -29,35 +38,56 @@ function standard.setup()
     ['{'] = '}',
     ['<'] = '>',
   }) do
-    insx.add(close, require('insx.recipe.jump_next')({
-      jump_pat = {
-        [[\%#]] .. esc(close) .. [[\zs]]
-      },
-    }))
-    insx.add(open, require('insx.recipe.auto_pair')({
-      open = open,
-      close = close,
-    }))
-    insx.add('<BS>', require('insx.recipe.delete_pair')({
-      open_pat = esc(open),
-      close_pat = esc(close),
-    }))
-    insx.add('<Space>', require('insx.recipe.pair_spacing').increase({
-      open_pat = esc(open),
-      close_pat = esc(close),
-    }))
-    insx.add('<BS>', require('insx.recipe.pair_spacing').decrease({
-      open_pat = esc(open),
-      close_pat = esc(close),
-    }))
-    insx.add('<CR>', require('insx.recipe.fast_break')({
-      open_pat = esc(open),
-      close_pat = esc(close),
-      split = true,
-    }))
-    insx.add('<C-]>', require('insx.recipe.fast_wrap')({
-      close = close
-    }))
+    insx.add(
+      close,
+      require('insx.recipe.jump_next')({
+        jump_pat = {
+          [[\%#]] .. esc(close) .. [[\zs]],
+        },
+      })
+    )
+    insx.add(
+      open,
+      require('insx.recipe.auto_pair')({
+        open = open,
+        close = close,
+      })
+    )
+    insx.add(
+      '<BS>',
+      require('insx.recipe.delete_pair')({
+        open_pat = esc(open),
+        close_pat = esc(close),
+      })
+    )
+    insx.add(
+      '<Space>',
+      require('insx.recipe.pair_spacing').increase({
+        open_pat = esc(open),
+        close_pat = esc(close),
+      })
+    )
+    insx.add(
+      '<BS>',
+      require('insx.recipe.pair_spacing').decrease({
+        open_pat = esc(open),
+        close_pat = esc(close),
+      })
+    )
+    insx.add(
+      '<CR>',
+      require('insx.recipe.fast_break')({
+        open_pat = esc(open),
+        close_pat = esc(close),
+        split = true,
+      })
+    )
+    insx.add(
+      '<C-]>',
+      require('insx.recipe.fast_wrap')({
+        close = close,
+      })
+    )
   end
 end
 
---
+++ lua/insx/preset/standard.spec.lua
@@ -0,0 +1,47 @@
+local spec = require('insx.spec')
+
+describe('insx.preset.standard', function()
+  it('should work', function()
+    require('insx.preset.standard').setup()
+
+    -- quotes.
+    for _, quote in ipairs({ '"', "'", '`' }) do
+      -- autopairs.
+      spec.assert('|', quote, ('%s|%s'):format(quote, quote))
+      -- autopairs (disabled if escaped).
+      spec.assert('\\|', quote, ('\\%s|'):format(quote))
+      -- jumpout.
+      spec.assert(('%s|%s'):format(quote, quote), quote, ('%s%s|'):format(quote, quote))
+      -- delete.
+      spec.assert(('%s|%s'):format(quote, quote), '<BS>', '|')
+    end
+
+    -- pairs.
+    for open, close in pairs({
+      ['('] = ')',
+      ['['] = ']',
+      ['{'] = '}',
+      ['<'] = '>',
+    }) do
+      -- autopairs.
+      spec.assert('|', open, ('%s|%s'):format(open, close))
+      -- jumpout.
+      spec.assert(('%s|%s'):format(open, close), close, ('%s%s|'):format(open, close))
+      -- delete.
+      spec.assert(('%s|%s'):format(open, close), '<BS>', '|')
+      -- increase space.
+      spec.assert(('%s|%s'):format(open, close), '<Space>', ('%s | %s'):format(open, close))
+      -- decrease space.
+      spec.assert(('%s | %s'):format(open, close), '<BS>', ('%s|%s'):format(open, close))
+      -- break.
+      spec.assert(('%s|%s'):format(open, close), '<CR>', {
+        open,
+        '  |',
+        close
+      })
+      -- wrap.
+      spec.assert(('%s|%sconsole.log(foo, bar)'):format(open, close), '<C-]>', ('%sconsole.log(foo, bar)|%s'):format(open, close))
+    end
+
+  end)
+end)
