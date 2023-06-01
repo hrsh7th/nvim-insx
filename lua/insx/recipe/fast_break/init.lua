5ae02d0e57820c1ca30d0d21baad2b8e1edaf040
hrsh7th <hrsh7th@users.noreply.github.com>
2023-06-01 02:03:34 +0000

    Auto generated commit

--- lua/insx/recipe/fast_break/init.lua
+++ lua/insx/recipe/fast_break/init.lua
@@ -9,26 +9,32 @@ local insx = require('insx')
 ---@return insx.RecipeSource
 local function fast_break(option)
   if option.split then
-    vim.deprecate("fast_break option.split", 'option.html_attrs and option.arguments', '0', 'nvim-insx')
+    vim.deprecate('fast_break option.split', 'option.html_attrs and option.arguments', '0', 'nvim-insx')
   end
 
   local recipes = {}
 
-  table.insert(recipes, require('insx.recipe.fast_break.basic')({
-    open_pat = option.open_pat,
-    close_pat = option.close_pat,
-    indent = option.indent,
-  }))
+  table.insert(
+    recipes,
+    require('insx.recipe.fast_break.basic')({
+      open_pat = option.open_pat,
+      close_pat = option.close_pat,
+      indent = option.indent,
+    })
+  )
 
   if option.split or option.html_attrs then
     table.insert(recipes, require('insx.recipe.fast_break.html_attrs')({}))
   end
 
   if option.split or option.arguments then
-    table.insert(recipes, require('insx.recipe.fast_break.arguments')({
-      open_pat = option.open_pat,
-      close_pat = option.close_pat,
-    }))
+    table.insert(
+      recipes,
+      require('insx.recipe.fast_break.arguments')({
+        open_pat = option.open_pat,
+        close_pat = option.close_pat,
+      })
+    )
   end
   return insx.compose(recipes)
 end
