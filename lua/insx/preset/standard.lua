8f2ae661db3ed7d01fb8e4bab1810810d6d3c260
hrsh7th <629908+hrsh7th@users.noreply.github.com>
2023-06-30 22:35:31 +0900

    feat(preset.standard): improve markdown support

--- lua/insx/preset/standard.lua
+++ lua/insx/preset/standard.lua
@@ -6,122 +6,29 @@ local standard = {}
 
 ---@param config? insx.preset.standard.Config
 function standard.setup(config)
-  config = config or {}
-  standard.setup_insert_mode(config)
-  standard.setup_cmdline_mode(config)
+  standard.config = config or {}
+  standard.setup_insert_mode()
+  standard.setup_cmdline_mode()
 end
 
----@param config insx.preset.standard.Config
-function standard.setup_insert_mode(config)
-  -- quotes
-  for _, quote in ipairs({ '"', "'", '`' }) do
-    -- jump_out
-    insx.add(
-      quote,
-      require('insx.recipe.jump_next')({
-        jump_pat = {
-          [[\\\@<!\%#]] .. esc(quote) .. [[\zs]],
-        },
-      })
-    )
-
-    -- auto_pair
-    insx.add(
-      quote,
-      require('insx.recipe.auto_pair').strings({
-        open = quote,
-        close = quote,
-      })
-    )
-
-    -- delete_pair
-    insx.add(
-      '<BS>',
-      require('insx.recipe.delete_pair').strings({
-        open_pat = esc(quote),
-        close_pat = esc(quote),
-      })
-    )
-  end
-
-  -- pairs
-  for open, close in pairs({
-    ['('] = ')',
-    ['['] = ']',
-    ['{'] = '}',
-  }) do
-    -- jump_out
-    insx.add(
-      close,
-      require('insx.recipe.jump_next')({
-        jump_pat = {
-          [[\%#]] .. esc(close) .. [[\zs]],
-        },
-      })
-    )
-
-    -- auto_pair
-    insx.add(
-      open,
-      require('insx.recipe.auto_pair')({
-        open = open,
-        close = close,
-      })
-    )
-
-    -- delete_pair
-    insx.add(
-      '<BS>',
-      require('insx.recipe.delete_pair')({
-        open_pat = esc(open),
-        close_pat = esc(close),
-      })
-    )
-
-    -- spacing
-    if kit.get(config, { 'spacing', 'enabled' }, true) then
-      insx.add(
-        '<Space>',
-        require('insx.recipe.pair_spacing').increase({
-          open_pat = esc(open),
-          close_pat = esc(close),
-        })
-      )
-      insx.add(
-        '<BS>',
-        require('insx.recipe.pair_spacing').decrease({
-          open_pat = esc(open),
-          close_pat = esc(close),
-        })
-      )
+function standard.setup_insert_mode()
+  -- markdown `.
+  insx.add('`', {
+    enabled = function(ctx)
+      return ctx.match([[`\%#`]]) and ctx.filetype == 'markdown'
+    end,
+    action = function(ctx)
+      ctx.send('``<Left>')
+      ctx.send('``<Left>')
     end
+  })
+  insx.add('<CR>', require('insx.recipe.fast_break')({
+    open_pat = [[```\w*]],
+    close_pat = '```',
+    indent = 0,
+  }))
 
-    -- fast_break
-    if kit.get(config, { 'fast_break', 'enabled' }, true) then
-      insx.add(
-        '<CR>',
-        require('insx.recipe.fast_break')({
-          open_pat = esc(open),
-          close_pat = esc(close),
-          split = kit.get(config, { 'fast_break', 'split' }, nil),
-          html_attrs = kit.get(config, { 'fast_break', 'html_attrs' }, true),
-          arguments = kit.get(config, { 'fast_break', 'arguments' }, true),
-        })
-      )
-    end
-
-    -- fast_wrap
-    if kit.get(config, { 'fast_wrap', 'enabled' }, true) then
-      insx.add(
-        '<C-]>',
-        require('insx.recipe.fast_wrap')({
-          close = close,
-        })
-      )
-    end
-  end
-
-  -- tags.
+  -- html tag like.
   insx.add(
     '<CR>',
     require('insx.recipe.fast_break')({
@@ -129,46 +36,10 @@ function standard.setup_insert_mode(config)
       close_pat = insx.helper.search.Tag.Close,
     })
   )
-end
-
----@param config insx.preset.standard.Config
-function standard.setup_cmdline_mode(config)
-  if not kit.get(config, { 'cmdline', 'enabled' }, false) then
-    return
-  end
 
   -- quotes
   for _, quote in ipairs({ '"', "'", '`' }) do
-    -- jump_out
-    insx.add(
-      quote,
-      require('insx.recipe.jump_next')({
-        jump_pat = {
-          [[\\\@<!\%#]] .. esc(quote) .. [[\zs]],
-        },
-      }),
-      { mode = 'c' }
-    )
-
-    -- auto_pair
-    insx.add(
-      quote,
-      require('insx.recipe.auto_pair').strings({
-        open = quote,
-        close = quote,
-      }),
-      { mode = 'c' }
-    )
-
-    -- delete_pair
-    insx.add(
-      '<BS>',
-      require('insx.recipe.delete_pair').strings({
-        open_pat = esc(quote),
-        close_pat = esc(quote),
-      }),
-      { mode = 'c' }
-    )
+    standard.set_quote('i', quote)
   end
 
   -- pairs
@@ -177,36 +48,116 @@ function standard.setup_cmdline_mode(config)
     ['['] = ']',
     ['{'] = '}',
   }) do
-    -- jump_out
-    insx.add(
-      close,
-      require('insx.recipe.jump_next')({
-        jump_pat = {
-          [[\%#]] .. esc(close) .. [[\zs]],
-        },
-      }),
-      { mode = 'c' }
-    )
+    standard.set_pair('i', open, close)
+  end
+end
 
-    -- auto_pair
-    insx.add(
-      open,
-      require('insx.recipe.auto_pair')({
-        open = open,
-        close = close,
-      }),
-      { mode = 'c' }
-    )
+function standard.setup_cmdline_mode()
+  if not kit.get(standard.config, { 'cmdline', 'enabled' }, false) then
+    return
+  end
 
-    -- delete_pair
-    insx.add(
-      '<BS>',
-      require('insx.recipe.delete_pair')({
+  -- quotes
+  for _, quote in ipairs({ '"', "'", '`' }) do
+    standard.set_quote('c', quote)
+  end
+
+  -- pairs
+  for open, close in pairs({
+    ['('] = ')',
+    ['['] = ']',
+    ['{'] = '}',
+  }) do
+    standard.set_pair('c', open, close)
+  end
+end
+
+---@param mode string
+---@param quote string
+---@param withs? insx.Override[]
+function standard.set_quote(mode, quote, withs)
+  withs = withs or {}
+  local option = { mode = mode, }
+
+  -- jump_out
+  insx.add(quote, insx.with(require('insx.recipe.jump_next')({
+    jump_pat = {
+      [[\\\@<!\%#]] .. esc(quote) .. [[\zs]],
+    },
+  }), withs), option)
+
+  -- auto_pair
+  insx.add(quote, insx.with(require('insx.recipe.auto_pair').strings({
+    open = quote,
+    close = quote,
+  }), withs), option)
+
+  -- delete_pair
+  insx.add('<BS>', insx.with(require('insx.recipe.delete_pair').strings({
+    open_pat = esc(quote),
+    close_pat = esc(quote),
+  }), withs), option)
+end
+
+---@param mode string
+---@param open string
+---@param close string
+---@param withs? insx.Override[]
+function standard.set_pair(mode, open, close, withs)
+  withs = withs or {}
+  local option = { mode = mode, }
+
+  -- jump_out
+  insx.add(close, insx.with(require('insx.recipe.jump_next')({
+    jump_pat = {
+      [[\%#]] .. esc(close) .. [[\zs]],
+    },
+  }), withs), option)
+
+  -- auto_pair
+  insx.add(open, insx.with(require('insx.recipe.auto_pair')({
+    open = open,
+    close = close,
+  }), withs), option)
+
+  -- delete_pair
+  insx.add('<BS>', insx.with(require('insx.recipe.delete_pair')({
+    open_pat = esc(open),
+    close_pat = esc(close),
+  }), withs), option)
+
+  -- spacing
+  if kit.get(standard.config, { 'spacing', 'enabled' }, true) then
+    insx.add('<Space>', insx.with(require('insx.recipe.pair_spacing').increase({
+      open_pat = esc(open),
+      close_pat = esc(close),
+    }), withs), option)
+
+    insx.add('<BS>', insx.with(require('insx.recipe.pair_spacing').decrease({
+      open_pat = esc(open),
+      close_pat = esc(close),
+    }), withs), option)
+  end
+
+  -- insert mode only.
+  if option.mode == 'i' then
+    -- fast_break
+    if kit.get(standard.config, { 'fast_break', 'enabled' }, true) then
+      insx.add('<CR>', insx.with(require('insx.recipe.fast_break')({
         open_pat = esc(open),
         close_pat = esc(close),
-      }),
-      { mode = 'c' }
-    )
+        split = kit.get(standard.config, { 'fast_break', 'split' }, nil),
+        html_attrs = kit.get(standard.config, { 'fast_break', 'html_attrs' }, true),
+        arguments = kit.get(standard.config, { 'fast_break', 'arguments' }, true),
+      }), withs), option)
+    end
+
+    -- fast_wrap
+    if kit.get(standard.config, { 'fast_wrap', 'enabled' }, true) then
+      insx.add('<C-]>', insx.with(require('insx.recipe.fast_wrap')({
+        close = close,
+      }), withs), option)
+    end
   end
 end
 
